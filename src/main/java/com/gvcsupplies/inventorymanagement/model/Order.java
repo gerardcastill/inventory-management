b2eb87fc05fd70c4d06623caa725759413e7a3dc
gcastillo <gcastillo.ent@gmail.com>
2024-02-27 18:53:44 -0600

    included order schema and created service package

--- src/main/java/com/gvcsupplies/inventorymanagement/model/Order.java
+++ src/main/java/com/gvcsupplies/inventorymanagement/model/Order.java
@@ -2,6 +2,8 @@ package com.gvcsupplies.inventorymanagement.model;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table (name = "order")
 public class Order {
@@ -9,5 +11,22 @@ public class Order {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "staff_id")
+    private Staff staff;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    // Join table that has list of products attached to an order_id
+    @ManyToMany
+    @JoinTable (
+            name = "order_product",
+            joinColumns = @JoinColumn (name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> products;
 
 }
